ad28d73fee498dffadb386e0d89e3c1e5a0d6085
suruaino <studymail4suru@gmail.com>
2025-05-12 18:44:40 +0100

    Added responsiveness to the header's search bar

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,7 +5,7 @@ import styles from './header.module.css';
 
 const Header = () => {
     return(
-        <div className={`flex justify-between items-center px-4 h-16 ${styles.header}`}>
+        <div className={`flex justify-between items-center px-6 md:px-12 h-16 ${styles.header}`}>
             <div className="logo text-[var(--background)]">
                 <code className='font-bold'>PSonic</code>
             </div>
--- src/components/ui/search-bar/SearchBarA.tsx
+++ src/components/ui/search-bar/SearchBarA.tsx
@@ -2,12 +2,10 @@ import styles from "./search-bar-a.module.css";
 
 const SearchBarA = () => {
   return (
-    <div className={`search-box ${styles.searchBarA}`}>
-      <div className="search-icon">
+    <div className={`search-box w-full h-14 md:h-auto md:w-[20rem] px-6 md:px-16 mt-[-1rem] flex absolute top-20 left-[50%] md:left-auto translate-x-[-50%] md:translate-x-0 md:static ${styles.searchBarA}`}>
 
-      </div>
-      <div className="search-input">
-        <input type="text" placeholder="Search Artists, Songs & Album" className="w-[20rem] h-8 px-4" />
+      <div className="search-input w-full h-full flex items-center self-center">
+        <input type="text" placeholder="Search Artists, Songs & Album" className="w-full h-8 px-4" />
       </div>
     </div>
   );
